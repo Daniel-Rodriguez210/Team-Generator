d46683de6217e20c6c63f264a47c70bd4bc17d28
Daniel Rodriguez <drod18@hotmail.com>
2020-11-27 18:24:38 -0600

    able to create fs html file

--- Assets/Develop/app.js
+++ Assets/Develop/app.js
@@ -11,42 +11,42 @@ let teamMembers = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function Employee () {
-inquirer.prompt([
+function beginBuild() {
+    inquirer.prompt([
         {
             /* Questions for person creating the team*/
-        type: "Input",
-        message: "What is your name?",
-        name: "Name"
+            type: "Input",
+            message: "What is your name?",
+            name: "Name"
 
         },
         {
-        type: "Input",
-        message: "What is your ID?",
-        name: "ID"
+            type: "Input",
+            message: "What is your ID?",
+            name: "ID"
 
         },
         {
-        type: "Input",
-        message: "What is your Email?",
-        name: "Email"
+            type: "Input",
+            message: "What is your Email?",
+            name: "Email"
 
         },
         {
-        type: "Input",
-        message: "What is your Role?",
-        name: "Role",
-            
-    }])
+            type: "Input",
+            message: "What is your Role?",
+            name: "Role",
+
+        }])
         .then(function (data) {
             const name = data.name
             const id = data.id
-            const email = data.email 
+            const email = data.email
             const role = data.role
             const addition = new Employee(name, id, email, role)
             teamMembers.push(addition)
             addAnotherPerson();
-        }) 
+        })
 };
 
 function addAnotherPerson() {
@@ -54,67 +54,67 @@ function addAnotherPerson() {
         {
             type: "rawlist",
             message: "Would you like to add another person to your team?",
-            choices: ["Yes, Manager","Yes, Engineer","Yes, Intern", "No"],
+            choices: ["Yes, Manager", "Yes, Engineer", "Yes, Intern", "No"],
             name: "newPersonData"
         }
     ])
-        .then(function(data) {
-            switch(data.newPersonData) {
+        .then(function (data) {
+            switch (data.newPersonData) {
                 case "Yes, Manager":
                     addManager();
                     break;
-                    
-                    case "Yes, Engineer":
+
+                case "Yes, Engineer":
                     addEngineer();
                     break;
 
-                    case "Yes, Intern":
+                case "Yes, Intern":
                     addIntern();
                     break;
 
-                    case "No":
+                case "No":
                     finishTeam();
-                    break;
+                    console.log(teamMembers)
             }
         })
-    };
+};
 
-    function addManager() {
-        inquirer.prompt ([
-            {
-                type: "Input",
-                message: "What is the manager's name?",
-                name: "name"
-            },
-            {
-                type: "Input",
-                message: "What is the manager's ID?",
-                name: "ID"
-            },
-            {
-                type: "Input",
-                message: "What is the manager's Email?",
-                name: "Email"
-            },
-            {
-                type: "Input",
-                message: "What is the manager's office number?",
-                name: "officenumber"
-            }
-        ])
-            .then(function(data) {
-                const name = data.name
-                const id = data.id
-                const email = data.email
-                const officenumber = data.officenumber
-                const addition = new Manager(name, id, email, officenumber)
-                teamMembers.push(addition)
-                addAnotherPerson()
-            })
-    } 
-    
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "Input",
+            message: "What is the manager's name?",
+            name: "name"
+        },
+        {
+            type: "Input",
+            message: "What is the manager's ID?",
+            name: "ID"
+        },
+        {
+            type: "Input",
+            message: "What is the manager's Email?",
+            name: "Email"
+        },
+        {
+            type: "Input",
+            message: "What is the manager's office number?",
+            name: "officenumber"
+        }
+    ])
+        .then(function (data) {
+            const name = data.name
+            const id = data.id
+            const email = data.email
+            const officenumber = data.officenumber
+            const addition = new Manager(name, id, email, officenumber)
+            teamMembers.push(addition)
+            addAnotherPerson()
+        })
+}
+
 function addEngineer() {
-    inquirer.prompt ([
+    inquirer.prompt([
         {
             type: "Input",
             message: "What is your engineer's name?",
@@ -134,10 +134,10 @@ function addEngineer() {
             type: "Input",
             message: "What is the engineer's ID?",
             name: "ID"
-    
-            }
+
+        }
     ])
-        .then(function(data) {
+        .then(function (data) {
             const name = data.name
             const username = data.username
             const email = data.email
@@ -146,10 +146,10 @@ function addEngineer() {
             teamMembers.push(addition)
             addAnotherPerson()
         })
-};    
+};
 
 function addIntern() {
-    inquirer.prompt ([
+    inquirer.prompt([
         {
             type: "Input",
             message: "What is your intern's name?",
@@ -159,7 +159,7 @@ function addIntern() {
             type: "Input",
             message: "What is the intern's ID?",
             name: "ID"
-    
+
         },
         {
             type: "Input",
@@ -171,25 +171,88 @@ function addIntern() {
             message: "What is the intern's school? ",
             name: "School"
         }
-        
+
     ])
-        .then(function(data) {
+        .then(function (data) {
             const name = data.name
             const id = data.id
             const email = data.email
-            const school = data.school 
+            const school = data.school
             const addition = new Intern(name, id, email, school)
             teamMembers.push(addition)
             addAnotherPerson()
         })
 };
 
+beginBuild();
+
+
 function finishTeam() {
-    console.log(teamMembers)
+    const htmlArray = []
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    `
+    htmlArray.push(html);
+
+    for (let i = 1; i < teamMembers.length; i++) {
+        let object = `
+        <div class="member-card">
+            <div class="card-top">
+                <h2>${teamMembers[i].name}</h2>
+                <h2>${teamMembers[i].role}</h2>
+            </div>
+            <div class="card-bottom">
+                <p>Employee ID: ${teamMembers[i].id}</p>
+                <p>Email: <a href="mailto:${teamMembers[i].email}">${teamMembers[i].email}</a>></p>
+        `
+        if (teamMembers[i].officenumber) {
+            object += `
+            <p>${teamMembers[i].officenumber}</p>
+            `
+        }
+        if (teamMembers[i].username) {
+            object += `
+            <p>GitHub: <a href="https://github.com/${teamMembers[i].username}">${teamMembers[i].username}</a></p>
+            `
+        }
+        if (teamMembers[i].school) {
+            object += `
+            <p>School: ${teamMembers[i].school}</p>
+            `
+        }
+        object += `
+        </div>
+        </div>
+        `
+        htmlArray.push(object)
+    }
+
+    const htmlEnd = `
+    </div>
+    </body>
+    </html>
+    `
+    htmlArray.push(htmlEnd);
+
+    fs.writeFile(`./rendered-html/${teamMembers[0]}.html`, htmlArray.join(""), function (err) {
+
+    })
+    console.log(finishTeam)
 };
 
 
-Employee();
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
